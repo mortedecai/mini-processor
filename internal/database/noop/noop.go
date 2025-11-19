bce794258a73645ca642b510c75497510a943e6b
Dan Taylor <dan.taylor@rising-phoenix.software>
2025-11-19 09:33:36 -0500

    Add the no-op database and testing

--- internal/database/noop/noop.go
+++ internal/database/noop/noop.go
@@ -1,1 +1,29 @@
 package noop
+
+import (
+	"github.com/censys/scan-takehome/internal/database"
+	"github.com/censys/scan-takehome/internal/database/config"
+	"github.com/censys/scan-takehome/internal/database/dal"
+	"github.com/censys/scan-takehome/internal/database/models"
+	"go.uber.org/zap"
+)
+
+const (
+	DB_NOOP = "noop"
+)
+
+func init() {
+	database.RegisterDB(DB_NOOP, New)
+}
+
+// DBNoop is a no-operation database implementation that satisfies the dal.Scan interface.
+type DBNoop struct{}
+
+func (db *DBNoop) Close()                           {}
+func (db *DBNoop) Upsert(_ *models.ScanEntry) error { return nil }
+
+// New creates a new instance of the noop database.
+func New(_ *config.Config) (dal.Scan, error) {
+	zap.S().Warn("using noop database, no data will be stored")
+	return &DBNoop{}, nil
+}
